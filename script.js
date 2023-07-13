9e5e13e3fae7a9692e1d6c30c4c17324aa188601
jaimechazatar <jaimefer19@hotmail.com>
2023-07-13 00:24:09 -0500

    Funcionalidad presion

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function convertirTemperatura() {
     var fahrenheit = (celsius * 9 / 5) + 32;
     document.getElementById("resultado").innerHTML = celsius + " grados Celsius son equivalentes a " + fahrenheit + " grados Fahrenheit.";
 }
+
+function convertirPresion() {
+    var valorPascal = document.getElementById("valorPascal").value;
+    var valorBar = valorPascal / 100000;
+    document.getElementById("resultado2").innerHTML = valorPascal + " pascales son equivalentes a " + valorBar + " Bar.";
+}
